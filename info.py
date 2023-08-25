1c8fcd954bb33dc30a2ce4ea3a83526316a64203
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2023-08-25 11:43:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,16 +62,6 @@ lazy_renamers = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrena
 LAZY_RENAMERS = (lazy_renamers + ADMINS) if lazy_renamers else []
 REQ_CHANNEL = int(environ.get('REQ_CHANNEL', '-1001829705047'))
 
-#ai
-OPENAI_API = environ.get("OPENAI_API","")
-AI = is_enabled((environ.get("AI","True")), False)
-LAZY_AI_LOGS = int(environ.get("LAZY_AI_LOGS","-1001900179567")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of LazyPrincess ]
-# Requested Content template variables ---
-ADMIN_USRNM = environ.get('ADMIN_USRNM','Moneycaseadmin') # WITHOUT @
-MAIN_CHANNEL_USRNM = environ.get('MAIN_CHANNEL_USRNM','Moneycaseofficial') # WITHOUT @
-DEV_CHANNEL_USRNM = environ.get('DEV_CHANNEL_USRNM','krsbots') # WITHOUT @
-LAZY_YT_HANDLE = environ.get('LAZY_YT_HANDLE','Playitlinksofficial')  # WITHOUT @ [  add only handle - don't add full url  ] 
-MOVIE_GROUP_USERNAME = environ.get('MOVIE_GROUP_USERNAME', "+OCEl4k24o8FlNzJl") #[ without @ ]
 
 # Url Shortner
 URL_MODE = is_enabled((environ.get("URL_MODE","True")), False)
